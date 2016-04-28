51e67126887d2d5755c859dee982430c3ccc1501
jayvee-Air <jayveehe@gmail.com>
2016-04-28 15:51:29 +0800

    delete old java files

--- src/hell.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ITTC-Jayvee on 2015/6/26.
- */
-public class hell {
-    public static void main(String a[]){
-        System.out.println("fuck");
-    }
-}
--- src/test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ITTC-Jayvee on 2015/6/26.
- */
-public class test {
-    public static void main(String a[]) {
-        System.out.println("fuck");
-    }
-}
